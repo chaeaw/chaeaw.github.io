38a2e34945b1d979fe71ff64f6b06d8ef3858d78
chaeaw04 <chaeaw04@gmail.com>
2023-08-13 00:10:21 +0900

    [update] 레이아웃 및 디자인 변경중

--- js/background.js
+++ js/background.js
@@ -1,16 +1,10 @@
-const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg"
-];
-const bgBox = document.querySelector("#bgBox")
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const imgWrap = document.querySelector(".imgWrap");
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.classList.add("bg");
 bgImage.src = `img/${chosenImage}`;
 
-bgBox.appendChild(bgImage);
+imgWrap.appendChild(bgImage);
--- js/greeting.js
+++ js/greeting.js
@@ -1,52 +1,61 @@
-const loginForm = document.querySelector("#login_form")
-const loginInput = document.querySelector("#login_form input")
-const greeting = document.querySelector("#greeting")
+const loginBox = document.querySelector("#formBox");
+const loginForm = document.querySelector(".login_form");
+const loginInput = document.querySelector(".login_form .name");
+const greetingBox = document.querySelector("#greetingBox");
+const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 // string으로만 이루어진 변수의 경우 대문자로만 쓰기도 함. 비교적 중요도가 떨어지기에. , 오타로 에러가 발생할 경우 알기 쉽게 변수에 담아주는 것이 좋음.
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginBox.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY,username);
+  localStorage.setItem(USERNAME_KEY, username);
   //setItem("key name", value)
   paintGreetings(username);
 }
 
-function reset(){
+function reset(e) {
+  e.preventDefault();
   localStorage.removeItem(USERNAME_KEY);
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit",onLoginSubmit);
-  greeting.classList.add(HIDDEN_CLASSNAME);
+  loginBox.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+  greetingBox.classList.add(HIDDEN_CLASSNAME);
   loginInput.value = "";
 }
 
-function logout(){
-const logOutBtn = document.createElement("button");
-logOutBtn.innerText = "change name";
+function logout() {
+  const logOutBtn = document.createElement("button");
+  logOutBtn.classList.add("button__change");
+  logOutBtn.innerText = "change name";
 
-logOutBtn.addEventListener("click",reset);
-greeting.appendChild(logOutBtn);
+  logOutBtn.addEventListener("click", reset);
+  greeting.appendChild(logOutBtn);
 }
 
-function paintGreetings(username){
+function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}`;
-  // string이랑 변수를 한 string안에서 작성하는 법! 작은 따옴표('') 아니고 백틱(``)임! 변수는 ${}안에 담아줄 것.
-  greeting.classList.remove(HIDDEN_CLASSNAME);  
+  greetingBox.classList.remove(HIDDEN_CLASSNAME);
   logout();
 }
 
-
-
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit",onLoginSubmit);
+if (savedUsername === null) {
+  loginBox.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
+}
 
-};
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+});
 
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.name !== null) {
+    greetingBox.classList.add(HIDDEN_CLASSNAME);
+  }
+});
