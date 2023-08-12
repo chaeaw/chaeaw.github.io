f0f1cdcad460b3f0e2defd9c458e9302dd370677
chaeaw04 <chaeaw04@gmail.com>
2023-08-13 00:09:42 +0900

    [feat] check시 취소선 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -1,21 +1,32 @@
-const toDoForm =  document.querySelector("#todo_form");
-const toDoInput = toDoForm.querySelector("input")
+const toDoForm = document.querySelector("#todo_form");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo_list");
 
-const TODOS_KEY = "todos"
+const TODOS_KEY = "todos";
 
 let toDos = [];
 
-function saveToDos(){
+function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
-  const clickedLi = event.target.parentElement;
-  clickedLi.remove();
+  const parentBox = event.target.parentElement;
+  const deleteLi = parentBox.parentElement;
+  deleteLi.remove();
 
-  toDos = toDos.filter((toDo) => toDo.id !== parseInt(clickedLi.id));
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteLi.id));
   saveToDos();
+}
+
+function checkToDo(event) {
+  const checkedSpan = event.target.parentNode.parentNode.firstChild;
+  checkedSpan.classList.toggle("check");
+  if (checkedSpan.classList.contains("check")) {
+    event.target.innerText = "✅";
+  } else {
+    event.target.innerText = "☑️";
+  }
 }
 
 function paintToDo(newTodo) {
@@ -23,38 +34,42 @@ function paintToDo(newTodo) {
   li.id = newTodo.id;
   const span = document.createElement("span");
   span.innerText = newTodo.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
+  const buttonBox = document.createElement("div");
+  buttonBox.classList.add("buttonBox");
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteBtn");
+  deleteButton.innerText = "❌";
+  const checkButton = document.createElement("button");
+  checkButton.classList.add("checkBtn");
+  checkButton.innerText = "☑️";
 
-  button.addEventListener("click", deleteToDo);
+  deleteButton.addEventListener("click", deleteToDo);
+  checkButton.addEventListener("click", checkToDo);
   li.appendChild(span);
-  li.appendChild(button);
+  li.appendChild(buttonBox);
+  buttonBox.appendChild(checkButton);
+  buttonBox.appendChild(deleteButton);
 
   toDoList.appendChild(li);
-
-
 }
 
-
-function handleToDoSubmit(event){
+function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  toDoInput.value = ""
+  toDoInput.value = "";
   const newTodoObj = {
-    text:newTodo,
+    text: newTodo,
     id: Date.now(),
   };
   toDos.push(newTodoObj);
   paintToDo(newTodoObj);
   saveToDos();
-
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
-if(savedToDos !== null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
